1fc7350b5489b50add217eff92222f063193bf33
Tim Sawtelle <timsawtelle@gmail.com>
2011-05-14 20:47:03 -0500

    price validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ActiveRecord::Base
   
   validates :title, :description, :image_url,  :presence => true
   
+  validates_numericality_of :price, :on => :create, :message => "is not a number"
+  
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  
 end
