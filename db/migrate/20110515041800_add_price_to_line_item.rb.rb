4894bb3b4ea7ba27b78c07dcb69512380cd97c1c
Tim Sawtelle <timsawtelle@gmail.com>
2011-05-15 00:45:03 -0500

    cart in sidebar - post ajax

--- db/migrate/20110515041800_add_price_to_line_item.rb.rb
+++ db/migrate/20110515041800_add_price_to_line_item.rb.rb
@@ -6,11 +6,15 @@
 # We make no guarantees that this code is fit for any purpose. 
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
-module ApplicationHelper
-  def hidden_div_if(condition, attributes = {}, &block)
-    if condition
-      attributes["style"] = "display: none"
+class AddPriceToLineItem < ActiveRecord::Migration
+  def self.up
+    add_column :line_items, :price, :decimal
+    LineItem.all.each do |li|
+      li.price = li.product.price
     end
-    content_tag("div", attributes, &block)
+  end
+
+  def self.down
+    remove_column :line_items, :price
   end
 end
