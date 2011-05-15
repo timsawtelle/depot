8c7144d467a32bd5c501f68c7912a2ae61b8ae45
Tim Sawtelle <timsawtelle@gmail.com>
2011-05-14 22:27:57 -0500

    formatting

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ActiveRecord::Base
   
+  default_scope :order => 'title'
+    
   validates :title, :description, :image_url,  :presence => true
   
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
@@ -10,5 +12,5 @@ class Product < ActiveRecord::Base
     :with => %r{\.(gif|jpg|png)$}i,
     :message => 'must be a URL for GIF, JPG or PNG image.'
   }
-  
+    
 end
