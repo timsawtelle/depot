8c99ad2042b563f669ed5914a6871e11579b3f8e
Tim Sawtelle <timsawtelle@gmail.com>
2011-05-17 09:00:48 -0500

    mail phase 1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,7 +23,7 @@ Depot::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
   
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :test
   
 end
 
